0936e32ba9041c88c97fcdfbddba5745f28bf08a
xinxin <xinxinjin89@gmail.com>
2014-04-09 12:42:42 -0700

    modify AWS config

--- LocalScripts/AWSCredentials.py
+++ LocalScripts/AWSCredentials.py
@@ -1,7 +1,14 @@
 ### AWS credentials: ####
-aws_access_key_id='AKIAI7AWJT7RBAPJX6WQ'
-aws_secret_access_key='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'
-keyPairFile='/Users/yoavfreund/.ssh/YoavsKeypair.pem' # name of local file storing keypair
-key_name="YoavsKeypair"         # name of keypair on AWS
-security_groups=['YoavsWork']
+# Change entries here to match your own #
+# Get values for the first two entries from the "Security crediential" Tab
+# Under in the menu under your name.
+aws_access_key_id='AKIAJGE3LE6POOXGJTBA'
+aws_secret_access_key='jfDF9hs/I9fizs/6XV+fx0AkiOqXs4F7q7CE/lzt'
+# Get the Keypair in the EC2 Dashboard page.
+#keyPairFile="~/.ssh/aws_key.pem" # name of file keeping local key
+keyPairFile="/Users/xinxin/.ssh/aws_key.pem" # name of file keeping local key
+key_name="aws_key" # name of keypair (not name of file where key is stored)
+# Set the security group On the EC2 page (You will need to add IP addresses if
+# you want to connect from a place previously unauthorized.
+security_groups=['xinxin']
 ### End of AWS credentials ####
