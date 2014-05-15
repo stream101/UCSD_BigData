8466b28ab105adbbd169d0c017b571de7d9a4e15
xinxin <xinxinjin89@gmail.com>
2014-05-15 01:06:01 -0700

    aa

--- utils/make_Creds.py
+++ utils/make_Creds.py
@@ -1,9 +1,9 @@
 import pickle
-vault=''
+vault='/Users/xinxin/Course/bigdata/Vault'
 with open(vault+'/RootCred.pkl','wb') as pickle_file:
-    pickle.dump({'ID':ID,'key_id':key_id,'secret_key':secret_key,\
-                 'password':password,\
-                 'ssh_key_name':ssh_key_name,\
-                 'ssh_key_pair_file':ssh_key_pair_file,\
-                 'security_groups':security_groups}
+    pickle.dump({'ID':'x7jin','key_id':'AKIAITGKSGQB3ICTPZWA','secret_key':'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',\
+                 'password':'123',\
+                 'ssh_key_name':'aws_key',\
+                 'ssh_key_pair_file':'/Users/xinxin/.ssh/aws_key.pem',\
+                 'security_groups':'xinxin'}
                 ,pickle_file)
